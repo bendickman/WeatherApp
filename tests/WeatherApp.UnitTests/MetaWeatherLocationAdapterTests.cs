f31fd40c06c82337fda1b4ec27bae5d44801db29
bendickman <ben.dickman@gmail.com>
2021-12-22 17:11:57 +0000

    add unit tests for MetaWeatherLocationAdapter

---
+++ tests/WeatherApp.UnitTests/MetaWeatherLocationAdapterTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using WeatherApp.Infrastructure.Common.Adapters;
+using WeatherApp.Infrastructure.Common.Entities;
+
+namespace WeatherApp.UnitTests
+{
+    public class MetaWeatherLocationAdapterTests
+    {
+        private MetaWeatherLocationAdapter _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new MetaWeatherLocationAdapter();
+        }
+
+        [Test]
+        public void Convert_Returns_Empty_Collection_When_MetaWeather_Response_Is_Empty()
+        {
+            //Arrange
+            var metaWeatherResponse = new List<MetaWeatherLocation>();
+
+            //Act
+            var locations = _sut.Convert(metaWeatherResponse);
+
+            //Assert
+            Assert.IsFalse(locations.Any(), "Location collection is not empty");
+        }
+
+        [Test]
+        public void Convert_Returns_Empty_Collection_When_MetaWeather_Response_Is_Null()
+        {
+            //Act
+            var locations = _sut.Convert(null);
+
+            //Assert
+            Assert.IsFalse(locations.Any(), "Location collection is not empty");
+        }
+    }
+}
