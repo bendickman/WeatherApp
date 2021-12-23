ed0f9dfc50657f0210138b983f4b3644219cfbef
bendickman <ben.dickman@gmail.com>
2021-12-23 12:31:50 +0000

    re-factor Refit client to be used by the DI container

--- src/WeatherApp.Api/Startup.cs
+++ src/WeatherApp.Api/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using WeatherApp.Infrastructure;
 using WeatherApp.Infrastructure.Entities;
+using Refit;
+using WeatherApp.Infrastructure.Common.Interfaces;
+using System;
 
 namespace WeatherApp.Api
 {
@@ -32,6 +35,9 @@ namespace WeatherApp.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherApp.Api", Version = "v1" });
             });
+
+            services.AddRefitClient<IMetaWeatherApi>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(Configuration["MetaWeatherApi:BaseUrl"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/WeatherApp.Infrastructure/Common/Services/MetaWeatherLocationService.cs
+++ src/WeatherApp.Infrastructure/Common/Services/MetaWeatherLocationService.cs
@@ -1,30 +1,25 @@
-﻿using Microsoft.Extensions.Options;
-using Refit;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using WeatherApp.Domain.Entities;
 using WeatherApp.Infrastructure.Common.Adapters;
 using WeatherApp.Infrastructure.Common.Interfaces;
-using WeatherApp.Infrastructure.Entities;
 
 namespace WeatherApp.Infrastructure.Common.Services
 {
     public class MetaWeatherLocationService : ILocationService
     {
         private readonly MetaWeatherLocationAdapter _adapter;
-        private readonly IOptions<MetaWeatherApi> _options;
+        private readonly IMetaWeatherApi _weatherApiClient;
 
-        public MetaWeatherLocationService(IOptions<MetaWeatherApi> options, MetaWeatherLocationAdapter adapter)
+        public MetaWeatherLocationService(MetaWeatherLocationAdapter adapter, IMetaWeatherApi weatherApiClient)
         {
             _adapter = adapter;
-            _options = options;
+            _weatherApiClient = weatherApiClient;
         }
 
         public async Task<IEnumerable<Location>> GetLocations(string searchText)
         {
-            var metaWeatherClient = RestService.For<IMetaWeatherApi>(_options.Value.BaseUrl);
-
-            var results = await metaWeatherClient.GetLocations(searchText).ConfigureAwait(false);
+            var results = await _weatherApiClient.GetLocations(searchText).ConfigureAwait(false);
 
             return _adapter.Convert(results);
         }      
